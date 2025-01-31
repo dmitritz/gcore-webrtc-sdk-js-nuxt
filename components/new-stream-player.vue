f47c575c05ad4fba9524501fd0913b1b7d19e59b
Dmitrii Tkach <dmitrii.tkach@gcore.com>
2025-01-31 15:12:13 +0400

    Update video player

--- components/new-stream-player.vue
+++ components/new-stream-player.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
-import { Player, PlayerEvent } from '@gcorevideo/player'
-import { TvIcon, SignalIcon, SignalSlashIcon } from '@heroicons/vue/24/outline';
+import { Player, PlayerEvent } from "@gcorevideo/player";
+import {
+  ErrorScreen,
+  SourceController,
+  SpinnerThreeBounce,
+} from "@gcorevideo/player-plugins";
+
+import { TvIcon, SignalIcon, SignalSlashIcon } from "@heroicons/vue/24/outline";
 import useStream from "~/composables/use-stream";
 
-const container = ref<HTMLDivElement>()
-const playing = ref(false)
+Player.registerPlugin(ErrorScreen);
+Player.registerPlugin(SourceController);
+Player.registerPlugin(SpinnerThreeBounce);
+
+const container = ref<HTMLDivElement>();
+const playing = ref(false);
 
 onMounted(async () => {
   const stream = await useStream();
@@ -19,28 +29,42 @@ onMounted(async () => {
           retryIntervals: {
             MPD: 1000,
           },
-        }
+        },
       },
       debug: true,
       mute: true,
+      spinner: {
+        showOnError: true,
+        showOnStart: true,
+      },
       strings: {},
       sources: stream.value.sources,
-    })
+    });
     player.on(PlayerEvent.Play, () => {
-      playing.value = true
-    })
+      playing.value = true;
+    });
     setTimeout(() => {
-      player.attachTo(container.value!)
-      playing.value = true
-    }, 0)
-  }, 3000)
-})
+      player.attachTo(container.value!);
+      playing.value = true;
+    }, 0);
+  }, 3000);
+});
 </script>
 
 <template>
-  <div class="w-full max-w-screen-md aspect-video relative bg-black" ref="container" id="player-container">
-    <tv-icon class="absolute bottom-2 left-2 w-4 h-4 z-10 stroke-white"/>
-    <signal-slash-icon class="absolute bottom-2 left-8 w-4 h-4 z-10 stroke-white animate-pulse" v-if="!playing" />
-    <signal-icon class="absolute bottom-2 left-8 w-4 h-4 z-10 stroke-white" v-else />
+  <div
+    class="w-full max-w-screen-md aspect-video relative bg-black"
+    ref="container"
+    id="player-container"
+  >
+    <tv-icon class="absolute bottom-2 left-2 w-4 h-4 z-10 stroke-white" />
+    <signal-slash-icon
+      class="absolute bottom-2 left-8 w-4 h-4 z-10 stroke-white animate-pulse"
+      v-if="!playing"
+    />
+    <signal-icon
+      class="absolute bottom-2 left-8 w-4 h-4 z-10 stroke-white"
+      v-else
+    />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/main.css', '@gcorevideo/player-plugins/dist/index.css'],
   devtools: { enabled: true },
   postcss: {
     plugins: {
